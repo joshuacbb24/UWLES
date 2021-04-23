eb33cf92ae8fa7f687f19eb1a15943e982108718
joshua <joshu@DESKTOP-LN7DOPJ>
2021-04-23 18:02:08 -0400

    trying to add notification sound and user images to chat

--- app/static/app/scripts/default_avatar.js
+++ app/static/app/scripts/default_avatar.js
@@ -1,40 +1,32 @@
 $(document).ready(function(){
+  
+  //
+  function showDefaultAvatar() {
 
-    let noAvatar = false;
-  
-  
-  let profileImg = $('#profile-pic')
-  let missingimg = $('#profile-pic-wrapper')
-  console.log(missingimg)
-  
-  // double check how to access "src" attribute
-  if (!profileImg.attr('src') && missingimg.length) {
-  missingimg.addClass('profile-pic')
-   var username = missingimg.data('username');
-    //var intial = JSON.stringify(username);
-   missingimg.text(username.charAt(0));
- /*  
-    var x = Math.floor(Math.random() * 256);
-    var y = Math.floor(Math.random() * 256);
-    var z = Math.floor(Math.random() * 256);
-    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-  */  
-    let bgAvatar = missingimg.data('bgcolor');
-    console.log(bgAvatar)
-    if (bgAvatar){
-       missingimg.css('background-color', bgAvatar)
-    }
-  /*  else {
-      //in db save value like 00ff00
-      <form id="background-post" method="post">
-          <input type="text" value="bgColor"></input>
-      </form>
-      document.getElementById("background-post").submit();
-      profileImg.style.backgroundColor = bgColor
-    }
-  */
-  
+        // This is using ids; have to refactor to use classes if showing multiple avatars on one page
+
+        //ids cannot be repeated on a page, but classes can
+        let profileImg = $('.profile-pic')
+        let missingimg = $('.profile-pic-wrapper')
+        console.log(missingimg)
+        
+        // double check how to access "src" attribute
+        if (!profileImg.attr('src') && missingimg.length) {
+        missingimg.addClass('profile-pic')
+        var username = missingimg.data('username');
+
+        missingimg.text(username.charAt(0));
+
+          let bgAvatar = missingimg.data('bgcolor');
+          console.log(bgAvatar)
+          if (bgAvatar){
+            missingimg.css('background-color', bgAvatar)
+          }
+
+        }
   }
+
+  showDefaultAvatar();
   
   
   });
