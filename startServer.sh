34b9a9157dd9d245d6c420028752cabc1cb1147d
EC2 Default User <ec2-user@ip-172-31-17-72.us-east-2.compute.internal>
2021-05-06 18:32:26 +0000

    Added missing dep to requirements.txt and updated run scripts

---
+++ startServer.sh
@@ -0,0 +1,3 @@
+export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
+export REDIS_HOST="uwles21-redis-chat.z1pyuj.0001.use2.cache.amazonaws.com"
+/usr/bin/python3 manage.py runserver '0.0.0.0:8000'
