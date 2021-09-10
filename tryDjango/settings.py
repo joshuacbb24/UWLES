4d3826e3c92162c9f18f344c2dcb38d1f95120dd
joshuacbb24 <74516911+joshuacbb24@users.noreply.github.com>
2021-09-10 15:00:27 -0400

    Update settings.py

--- tryDjango/settings.py
+++ tryDjango/settings.py
@@ -92,20 +92,6 @@ CHANNEL_LAYERS = {
     },
 }
 
-
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": os.environ.get('REDIS_URL'),
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-            "CONNECTION_POOL_KWARGS": {
-                "ssl_cert_reqs": None
-            },
-        }
-    }
-}
-
 NOTIFY_USERS_ON_ENTER_OR_LEAVE_ROOMS = True
 
 MSG_TYPE_MESSAGE = 0  # For standard messages
