185f87e32b1b00ca88236c0670e451918b285584
joshuacbb24 <74516911+joshuacbb24@users.noreply.github.com>
2021-09-10 17:53:51 -0400

    Update settings.py

--- tryDjango/settings.py
+++ tryDjango/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import posixpath
-import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,7 +121,13 @@ MESSAGE_TYPES_LIST = [
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'TEST': {
+            'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3') #change jc made
+        }
+    }
 }
 
 # Password validation
