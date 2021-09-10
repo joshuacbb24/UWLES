a88d6e0ce748ad6177b592972cc3bb676027a8d0
joshua <joshu@DESKTOP-LN7DOPJ>
2021-09-10 15:52:56 -0400

    move from TLS

--- tryDjango/settings.py
+++ tryDjango/settings.py
@@ -80,7 +80,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'tryDjango.wsgi.application'
 ASGI_APPLICATION = 'tryDjango.asgi.application'
-REDIS_HOST = os.environ.get('REDIS_TLS_URL', 'redis://localhost:6379/0')
+REDIS_HOST = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')
 REDIS_PORT = os.environ.get('REDIS_PORT', 6379)
 CHANNEL_LAYERS = {
     'default': {
