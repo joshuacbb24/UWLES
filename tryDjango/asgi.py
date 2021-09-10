63a05b9501d9733660419b5c2750a242ec3ff20d
joshua <joshu@DESKTOP-LN7DOPJ>
2021-09-10 15:40:43 -0400

    fixed daphne file

--- tryDjango/asgi.py
+++ tryDjango/asgi.py
@@ -1,21 +1,21 @@
 # mysite/asgi.py
+
+
+
+
+
 import os
 import django
-
-from channels.auth import AuthMiddlewareStack
+from channels.routing import get_default_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from app import routing
-
-
-from channels.routing import get_default_application
-
-"""
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tryDjango.settings")
 django.setup()
-application = get_default_application()
-"""
+from app import routing
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tryDjango.settings")
+from channels.auth import AuthMiddlewareStack
+
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
