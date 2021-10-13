bf30ed6b5af7ea31159a31c8eba94531b28b27b6
joshuacbb24 <74516911+joshuacbb24@users.noreply.github.com>
2021-10-13 14:48:54 -0400

    Update default_avatar.js

--- static_files/app/scripts/default_avatar.js
+++ static_files/app/scripts/default_avatar.js
@@ -40,7 +40,7 @@ function showTagAvatars() {
     // This is using ids; have to refactor to use classes if showing multiple avatars on one page
 
     //ids cannot be repeated on a page, but classes can
-    let missingimages = $('.tagify__dropdown__item__avatar-wrap')
+    let missingimages = $('.tagify__tag__avatar-wrap')
 
     // double check how to access "src" attribute
     for (var i = 0; i < missingimages.length; i++) {
@@ -48,9 +48,39 @@ function showTagAvatars() {
         let profileimg = missingimg.find("img")
         if (!profileimg.attr('src')) {
             var username = missingimg.data('username');
+            if (username) {
+                missingimg.text(username.charAt(0));
+            }
+            let bgAvatar = missingimg.data('bgcolor');
+            if (bgAvatar) {
+                missingimg.css('background-color', bgAvatar)
+            }
 
-            missingimg.text(username.charAt(0));
+        }
+    }
 
+}
+function showDropdownAvatars() {
+
+    // This is using ids; have to refactor to use classes if showing multiple avatars on one page
+
+    //ids cannot be repeated on a page, but classes can
+    let missingimages = $('.tagify__dropdown__item__avatar-wrap')
+
+    // double check how to access "src" attribute
+    for (var i = 0; i < missingimages.length; i++) {
+        let missingimg = $(missingimages[i])
+        let profileimg = missingimg.find("img")
+        if (!profileimg.attr('src')) {
+            let username = missingimg.data('username');
+            if (username) {
+                if (missingimg.text().length <= 0) {
+                    missingimg.append(username.charAt(0));
+                }
+                else {
+
+                }
+            }
             let bgAvatar = missingimg.data('bgcolor');
             if (bgAvatar) {
                 missingimg.css('background-color', bgAvatar)
