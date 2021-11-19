bd81782a63190417a9f565e69226bbfdff257d93
joshuacbb24 <74516911+joshuacbb24@users.noreply.github.com>
2021-11-19 16:49:23 -0500

    Update cal2.js

--- app/static/app/scripts/cal2.js
+++ app/static/app/scripts/cal2.js
@@ -228,7 +228,7 @@ else {
   $(".delete-modal").show();
 }
 });
-$("#confirmation").on('click', function () {
+$("#confirmation-of-delete").on('click', function () {
   $.ajax({
     type: "POST",
     url: "createevents/",
